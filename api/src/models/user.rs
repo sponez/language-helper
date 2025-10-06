75b440ebba7aeb10a9537bf829055867be14d025
sponez <ganzuk1998@gmail.com>
2025-10-06 07:51:23 +0400

    Documentation and tests

--- api/src/models/user.rs
+++ api/src/models/user.rs
@@ -1,3 +1,21 @@
+//! User data transfer object.
+//!
+//! This module defines the user model used for data transfer.
+
+/// Data transfer object representing a user.
+///
+/// This struct is used to transfer user information between different layers
+/// of the application.
+///
+/// # Examples
+///
+/// ```
+/// use api::models::user::UserDto;
+///
+/// let user = UserDto {
+///     // Initialize fields here
+/// };
+/// ```
 pub struct UserDto {
     // fields go here
 }
